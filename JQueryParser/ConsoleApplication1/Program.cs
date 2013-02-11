9cf2b4e9c4e53227f6720075dc2fd66eb4092ca3
Roland Zwaga <roland@stackandheap.com>
2013-02-11 22:13:47 +0100

    Deleted old classes that were replaced by the AS3CodeDom

--- JQueryParser/ConsoleApplication1/Program.cs
+++ JQueryParser/ConsoleApplication1/Program.cs
@@ -9,7 +9,6 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
-using ConsoleApplication1.output;
 
 namespace ConsoleApplication1
 {
